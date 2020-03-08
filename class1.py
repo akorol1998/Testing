1b709fa0b16ce3222cb2802fd25e6b877619beaa
Artem <akorol@unit.ua>
2020-03-08 17:10:27 +0200

    commit with my lab files

---
+++ class1.py
@@ -0,0 +1,79 @@
+
+class myClass():
+
+    @staticmethod
+    def computeFirst(x: float) -> float:
+        ''' This method takes an argument of type float and performs following computations:
+        y=x^4*4.769+x^3*4.159-x^2*2.745+x*4.503
+        value of an rgument should be in range of(-inf,2.853] and [88.069, +inf) '''
+
+        try:
+            if x > 2.853 and x < 88.069:
+                raise ValueError
+            y = x**(4*4.769) + x**(3*4.159) - x**(2*2.745) + x*3.317
+        except TypeError as error:
+            print(f"You got an {error}\nYou need float type")
+            y = None
+        except ValueError as error:
+            print(f"You got Wrong value {error}\n value should be in range of(-inf,2.853] and [88.069, +inf) float type")
+            y = None
+        return y
+
+    @staticmethod
+    def computeSecond(x):
+        ''' This method takes an argument of type float and performs following computations:
+        y=x^3*2.027-x^2*2.578+x*6.966
+        value of an rgument should be in range of(-inf,2.853] and [88.069, +inf) '''
+        try:
+            if x > 2.853 and x < 88.069:
+                raise ValueError
+            y = x**(3*2.027)-x**(2*2.578)+x*6.966
+        except TypeError as error:
+            print(f"You got an {error}\nYou need float type")
+            y = None
+        except ValueError as error:
+            print(f"You got Wrong value {error}\n value should be in range of(-inf,2.853] and [88.069, +inf) float type")
+            y = None
+        return y
+
+    @staticmethod
+    def computeThird(x):
+        ''' This method takes an argument of type float and performs following computations:
+        y=x^2*1.575+x*3.894
+        value of an rgument should be in range of(-inf,2.853] and [88.069, +inf) '''
+        try:
+            if x > 2.853 and x < 88.069:
+                raise ValueError
+            y = x**(2*1.575)+x*3.894
+        except TypeError as error:
+            print(f"You got an {error}\nYou need float type")
+            y = None
+        except ValueError as error:
+            print(f"You got Wrong value {error}\n value should be in range of(-inf,2.853] and [88.069, +inf) float type")
+            y = None
+        return y
+    
+    @staticmethod
+    def computeFourth(x):
+        ''' This method takes an argument of type float and performs following computations:
+        y=x*2.644
+        value of an rgument should be in range of(-inf,2.853] and [88.069, +inf) '''
+        try:
+            if x > 2.853 and x < 88.069:
+                raise ValueError
+            y = x*2.644
+        except TypeError as error:
+            print(f"You got an {error}\nYou need float type")
+            y = None
+        except ValueError as error:
+            print(f"You got Wrong value {error}\n value should be in range of(-inf,2.853] and [88.069, +inf) float type")
+            y = None
+        return y
+
+
+if "__main__" == __name__:
+	obj = myClass()
+	print(myClass.computeFirst(float("1.3")))
+	print(myClass.computeSecond(float("1.3")))
+	print(myClass.computeThird(float("1.3")))
+	print(myClass.computeFourth(float("1.3")))
